1e5b8ff340477655f89c690407f14673df4d0365
Tim Thomas <tim-t@hotmail.de>
2021-01-23 23:57:54 +0100

    Add dark mode toggle functionality

--- popup.js
+++ popup.js
@@ -1,1 +1,10 @@
 // The logic for the popup
+
+// EventListener for the on/off switch
+document.querySelector(".onoffswitch-checkbox").addEventListener("click", onoff);
+
+// Function for the on/off switch for dark/light mode
+function onoff() {
+    document.body.classList.toggle("dark-theme")
+    console.log("click: " + onoffswitch.checked)
+}
