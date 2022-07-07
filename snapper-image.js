0388788429d8f3ecc327e08bce7b28efbe402705
Tim Thomas <tim-t@hotmail.de>
2022-07-08 00:01:59 +0200

    feat(snapper-image): :art: add captured image page styling
    #6

--- snapper-image.js
+++ snapper-image.js
@@ -1,7 +1,9 @@
+'use strict';
+
 // Add listener that recieves the image with additional information
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log("Listener initialized")
-    console.log("message: ", message, " sender: ", sender, " sendResponse: ", sendResponse, " message.action: ", message.action)
+    console.log("message: ", message, " sender: ", sender, " sendResponse: ", sendResponse, " message.action: ", message.action, " message.filename:", message.filename)
 
     // chrome.tabs.query({ currentWindow: true, title: "Snapper Screenshot" }, function (tabs) {
     //     var tab = tabs[0];
@@ -13,15 +15,27 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         console.log("in if in snapper-image")
 
         var image = document.getElementById("capture_image")
+        var downloadButtonPNG = document.getElementById("download_png")
 
         console.log("image: ", image)
 
         // Scale down image
         // image.style.transform = 'scale(0.8)'
 
+        // Set image source as base64 image
         image.src = message.image
 
-        // Just for debugging
+        // Add zoom in/out when image clicked
+        image.onclick = () => {
+            console.log("image clicked")
+            image.classList.contains('zoomed_in') ?
+                image.classList.remove('zoomed_in') : image.classList.add('zoomed_in')
+        }
+
+        // Set href and download property on button to download image when clicked
+        downloadButtonPNG.href = message.image
+        downloadButtonPNG.download = message.filename
+
         sendResponse(JSON.stringify(message, null, 4) || true)
 
         // return true;
